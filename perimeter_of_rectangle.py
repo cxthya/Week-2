6c723565a5e2663d4a5ae83b8b0f760832780e2e
cxthya <calderonhernandezcinthya@gmail.com>
2026-03-30 15:02:01 -0400

    Create perimeter_of_rectangle.py

---
+++ perimeter_of_rectangle.py
@@ -0,0 +1,16 @@
+'''
+Cinthya Calderon-Hernandez
+CMSC 111
+Spring 2026
+Assignment 2: Perimeter of a Rectangle
+'''
+
+#Ask for user information and make to a float
+length = float(input("Enter the length of the rectanle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+#Calculate the perimeter
+perimeter = 2 * (length + width)
+
+#Display the result
+print(f"The perimeter of the rectangle is {perimeter}.")
